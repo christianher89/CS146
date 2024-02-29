c7179ff5170aab3c7ef5658bff8551923317fcec
christianher89 <131484670+christianher89@users.noreply.github.com>
2024-02-29 12:39:29 -0800

    Update HW6.java

--- HW/HW6/HW6.java
+++ HW/HW6/HW6.java
@@ -5,11 +5,9 @@ public class HW6 {
 	public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
 
-        // Sorting the array is a crucial step for this algorithm
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 2; i++) {
-            // Skip duplicate values for the fixed element
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
@@ -22,20 +20,16 @@ public class HW6 {
                 int sum = nums[left] + nums[right];
 
                 if (sum == target) {
-                    // Found a triplet
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
-                    // Skip duplicate values for the second element
                     while (left < right && nums[left] == nums[left + 1]) {
                         left++;
                     }
 
-                    // Skip duplicate values for the third element
                     while (left < right && nums[right] == nums[right - 1]) {
                         right--;
                     }
 
-                    // Move pointers
                     left++;
                     right--;
                 } else if (sum < target) {
@@ -53,8 +47,6 @@ public class HW6 {
         HW6 threeSum = new HW6();
         int[] nums = {-1, 0, 1, 2, -1, -4};
         List<List<Integer>> result = threeSum.threeSum(nums);
-
-        // Print the result
         for (List<Integer> triplet : result) {
             System.out.println(triplet);
         }
